2dc80903826b20cfc4621112f621ef78a3db1fa8
MarcoAntolini <marco.antolini2001@gmail.com>
2022-11-16 15:57:52 +0100

    fix

--- src/arduino/smart_cm/Component.h
+++ src/arduino/smart_cm/Component.h
@@ -6,10 +6,15 @@
 class Component : public IComponent
 {
 public:
+    Component(uint8_t pin, uint8_t mode);
     void setPin(uint8_t pin);
     void setMode(uint8_t mode);
     uint8_t getPin();
     uint8_t getMode();
+
+private:
+    uint8_t pin;
+    uint8_t mode;
 };
 
 #endif
--- src/arduino/smart_cm/IComponent.h
+++ src/arduino/smart_cm/IComponent.h
@@ -4,15 +4,10 @@
 class IComponent
 {
 public:
-    IComponent(uint8_t pin, uint8_t mode);
     virtual void setPin(uint8_t pin) = 0;
     virtual void setMode(uint8_t mode) = 0;
     virtual uint8_t getPin() = 0;
     virtual uint8_t getMode() = 0;
-
-private:
-    uint8_t pin;
-    uint8_t mode;
 };
 
 #endif
