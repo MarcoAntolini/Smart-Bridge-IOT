97bae2b8bf15f136964e7e8448d7a833f4f5aa5e
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2022-11-17 15:16:50 +0100

    task, scheduler

---
+++ src/arduino/kernel/Scheduler.h
@@ -0,0 +1,23 @@
+#ifndef __SCHEDULER__
+#define __SCHEDULER__
+
+#include "Task.h"
+
+#define MAX_TASKS 20
+
+class Scheduler
+{
+private:
+    int period;
+    int nTasks;
+    Task* task1PList[MAX_TASKS];
+    Task* task2PList[MAX_TASKS];
+
+public:
+    scheduler(int period);
+
+    virtual bool addTask(Task* task);
+    virtual void breakTask();
+};
+
+#endif
