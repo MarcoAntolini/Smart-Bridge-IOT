ded450208566241a533900fbce51b9c9b7a383bd
MarcoAntolini <marco.antolini2001@gmail.com>
2022-11-23 11:03:16 +0100

    fix servo motor

--- src/arduino/smart_cm/components/ServoMotorImpl.cpp
+++ src/arduino/smart_cm/components/ServoMotorImpl.cpp
@@ -3,11 +3,13 @@
 void ServoMotorImpl::open(int angle)
 {
     this->angle = angle;
+    this->write(angle);
 }
 
 void ServoMotorImpl::close()
 {
     this->angle = 0;
+    this->write(angle);
 }
 
 int ServoMotorImpl::getAngle()
--- src/arduino/smart_cm/components/ServoMotorImpl.h
+++ src/arduino/smart_cm/components/ServoMotorImpl.h
@@ -2,11 +2,16 @@
 #define __SERVOMOTORIMPL__
 
 #include "ServoMotor.h"
+#include <Servo.h>
 
-class ServoMotorImpl : public ServoMotor
+class ServoMotorImpl : public ServoMotor, public Servo
 {
 public:
-    ServoMotorImpl(uint8_t pin) : ServoMotor(pin) { this->angle = 0; };
+    ServoMotorImpl(uint8_t pin) : ServoMotor(pin)
+    {
+        this->attach(pin);
+        this->angle = 0;
+    };
     void open(int angle);
     void close();
     int getAngle();
